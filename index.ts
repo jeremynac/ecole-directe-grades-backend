47a509340bd0f804aee2686509acf61c1eca0b18
Jeremy Naccache <jeremy.naccache@epitech.eu>
2022-10-20 14:16:01 +0200

    Add frontend url

--- index.ts
+++ index.ts
@@ -15,6 +15,7 @@ const app = express()
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Origin', 'https://ecole-direct-grades.vercel.app')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   res.header('Access-Control-Allow-Credentials', 'true')
   next()
